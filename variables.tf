6173b492d4e63281b21256f6406a7cf907358994
vanyakara <147335690+vanyakara@users.noreply.github.com>
2025-06-24 11:59:57 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,27 @@ variable "admin_password" {
   type        = string
   sensitive   = true
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+  sensitive   = true
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+  sensitive   = true
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+  sensitive   = true
+}
