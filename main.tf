66b61e5531af1d3f19baa4366868b4de949129f5
vanyakara <147335690+vanyakara@users.noreply.github.com>
2025-06-24 11:57:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,9 +38,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
   network_interface_ids = [azurerm_network_interface.nic.id]
-
   admin_username = "azureuser"
   admin_password = var.admin_password
   disable_password_authentication = false
