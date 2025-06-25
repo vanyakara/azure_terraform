2eb65efa81ae61c5b98576aef3b663cd7ccb396b
vanyakara <147335690+vanyakara@users.noreply.github.com>
2025-06-25 13:01:03 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,41 @@
-variable "admin_password" {
-  description = "Password for the admin user"
-  type        = string
-  sensitive   = true
-}
-
 variable "subscription_id" {
   type        = string
-  description = "Azure Subscription ID"
-  sensitive   = true
+  description = "Azure subscription ID"
 }
 
 variable "client_id" {
   type        = string
-  description = "Azure Client ID"
-  sensitive   = true
+  description = "Azure client ID"
 }
 
 variable "client_secret" {
   type        = string
-  description = "Azure Client Secret"
+  description = "Azure client secret"
   sensitive   = true
 }
 
 variable "tenant_id" {
   type        = string
-  description = "Azure Tenant ID"
+  description = "Azure tenant ID"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Admin password for the virtual machines"
   sensitive   = true
+}
+
+variable "vms" {
+  type = map(object({
+    name = string
+  }))
+  description = "Map of virtual machines to create"
+  default = {
+    vm1 = {
+      name = "myVM1"
+    }
+    vm2 = {
+      name = "myVM2"
+    }
+  }
 }
